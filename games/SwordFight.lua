3198290d70de8e40316dc5e74e02c48cf185dad5
BigJoeyJoeyFunnies <140270124+BigJoeyJoeyFunnies@users.noreply.github.com>
2023-07-30 19:34:56 -0700

    Update SwordFight.lua

--- games/SwordFight.lua
+++ games/SwordFight.lua
@@ -1,7 +1,7 @@
 
 
 
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/BigJoeyJoeyFunnies/Luck/main/CustomLib')))()
 local Window = OrionLib:MakeWindow({Name = "Luck (Sword Fights)", HidePremium = false, SaveConfig = true, ConfigFolder = "Luckswordfighting"})
 
 
@@ -52,6 +52,73 @@ local Tab = Window:MakeTab({
 	PremiumOnly = false
 })
 
+local isTPSpeedEnabled = false
+local tpSpeed = 2 
+local tpDelay = 1 
+local lastPosition = nil 
+
+local function teleportForward()
+    local localPlayer = game.Players.LocalPlayer
+    local character = localPlayer.Character
+    if character and character:FindFirstChild("HumanoidRootPart") then
+        local humanoidRootPart = character.HumanoidRootPart
+        local forwardVector = humanoidRootPart.CFrame.lookVector
+        humanoidRootPart.CFrame = CFrame.new(humanoidRootPart.Position + forwardVector * tpSpeed)
+    end
+end
+
+Tab:AddSlider({
+    Name = "TP SPEED",
+    Min = 0,
+    Max = 10,
+    Default = 2,
+    ValueName = "Studs per Second",
+    Callback = function(Value)
+        tpSpeed = Value
+        if isTPSpeedEnabled and lastPosition then
+            teleportForward() 
+        end
+    end
+})
+
+Tab:AddSlider({
+    Name = "Delay Between TP",
+    Min = 0,
+    Max = 5,
+    Default = 1,
+    ValueName = "Seconds",
+    Callback = function(Value)
+        tpDelay = Value
+    end
+})
+
+Tab:AddToggle({
+    Name = "TP SPEED",
+    Default = false,
+    Callback = function(Value)
+        isTPSpeedEnabled = Value
+        lastPosition = nil -- Reset the last position when enabling the toggle
+
+        while isTPSpeedEnabled do
+            local localPlayer = game.Players.LocalPlayer
+            local character = localPlayer.Character
+
+            if character and character:FindFirstChild("HumanoidRootPart") then
+                local humanoidRootPart = character.HumanoidRootPart
+                local currentPosition = humanoidRootPart.Position
+
+                if lastPosition and (currentPosition - lastPosition).magnitude > 0.01 then
+                    teleportForward()
+                end
+
+                lastPosition = currentPosition
+            end
+
+            wait(tpDelay) 
+        end
+    end
+})
+
 
 Tab:AddButton({
 	Name = "Tp behind players!",
@@ -61,7 +128,43 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/BigJoeyJoeyFunnies/ro
   	end    
 })
 
+Tab:AddButton({
+    Name = "DeathPlayerTP",
+    Callback = function()
 
+        local function teleportToRandomPlayer()
+            local players = game.Players:GetPlayers()
+            local randomPlayer = players[math.random(1, #players)]
+
+            if randomPlayer and randomPlayer ~= game.Players.LocalPlayer then
+                local character = game.Players.LocalPlayer.Character
+                if character then
+                    local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+                    if humanoidRootPart then
+                        humanoidRootPart.CFrame = randomPlayer.Character.HumanoidRootPart.CFrame
+                    end
+                end
+            end
+        end
+
+        local localPlayer = game.Players.LocalPlayer
+        local character = localPlayer.Character
+        if character then
+            character:BreakJoints() -- Reset the character by breaking its joints (respawns the character)
+        end
+
+        -- Wait for the character to respawn
+        local characterAddedConnection
+        characterAddedConnection = localPlayer.CharacterAdded:Connect(function(respawnedCharacter)
+            characterAddedConnection:Disconnect() -- Disconnect the event to avoid multiple calls
+            wait(1) -- Wait for 1 second to ensure the respawn process completes
+            teleportToRandomPlayer() -- Teleport the player to a random player's location after respawn
+        end)
+
+        -- Teleport the player immediately after character reset (before respawn)
+        teleportToRandomPlayer()
+    end    
+})
 
 local Tab = Window:MakeTab({
 	Name = "MOVEMENT",
@@ -81,7 +184,7 @@ Tab:AddSlider({
 	Name = "Slider",
 	Min = 0,
 	Max = 100,
-	Default = 5,
+	Default = 16,
 	ValueName = "Speed",
 	Callback = function(Value)
 		local player = game.Players.LocalPlayer
@@ -95,7 +198,7 @@ Tab:AddSlider({
 	Name = "Slider",
 	Min = 0,
 	Max = 100,
-	Default = 5,
+	Default = 50,
 	ValueName = "Jump Power",
 	Callback = function(Value)
 		local player = game.Players.LocalPlayer
@@ -251,7 +354,7 @@ local function teleportToNextAlivePlayer()
 end
 
 Tab:AddToggle({
-    Name = "Auto Win",
+    Name = "Auto Win Toggle",
     Default = false,
     Callback = function(Value)
         isAutoWinEnabled = Value
