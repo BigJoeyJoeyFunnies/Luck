0eef265e307db038d0304f7cf06b2208e418b8b3
BigJoeyJoeyFunnies <140270124+BigJoeyJoeyFunnies@users.noreply.github.com>
2023-07-30 17:44:48 -0700

    Update MainLoder.lua

--- MainLoder.lua
+++ MainLoder.lua
@@ -22,5 +22,32 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/Si
 
 
 -- Skywars (Voxel)
+
+
   
-end
+-- sword fights 
+  
+elseif game.PlaceId == 8296410597 then -- Sword Fight Arena
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
+ 
+elseif game.PlaceId == 8810636550 then -- Kill others and flex your swords
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
+
+elseif game.PlaceId == 9377954133 then -- Mobile Sword Fights (Clasic)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
+  
+elseif game.PlaceId == 6130206252 then -- Mobile Sword Fights
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
+
+
+  
+  
+
+
+
+
+
