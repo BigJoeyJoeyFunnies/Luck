64793304f0cd911889d3e801470552d22c9c2bb3
BigJoeyJoeyFunnies <140270124+BigJoeyJoeyFunnies@users.noreply.github.com>
2023-07-30 17:34:58 -0700

    Create MainLoder.lua

---
+++ MainLoder.lua
@@ -0,0 +1,26 @@
+-- chat tags
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BigJoeyJoeyFunnies/roux/main/fix.lua"))()
+
+
+
+-- Bridge Duels
+if game.PlaceId == 11630038968 then -- lobby
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
+
+elseif game.PlaceId == 12011959048 then -- in game (solos)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
+
+
+
+-- Skywars(16bit)
+
+elseif game.PlaceId == 85549080 then -- in game (solos)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
+
+
+-- Skywars (Voxel)
+  
+end
