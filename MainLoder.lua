db33b18a6fb7d8fbdeae1124694c6948ab0f3775
BigJoeyJoeyFunnies <140270124+BigJoeyJoeyFunnies@users.noreply.github.com>
2023-07-30 17:49:22 -0700

    Update MainLoder.lua

--- MainLoder.lua
+++ MainLoder.lua
@@ -29,19 +29,19 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/Si
   
 elseif game.PlaceId == 8296410597 then -- Sword Fight Arena
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BigJoeyJoeyFunnies/roux/main/swordfighintg.lua"))()
  
 elseif game.PlaceId == 8810636550 then -- Kill others and flex your swords
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BigJoeyJoeyFunnies/roux/main/swordfighintg.lua"))()
 
 elseif game.PlaceId == 9377954133 then -- Mobile Sword Fights (Clasic)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BigJoeyJoeyFunnies/roux/main/swordfighintg.lua"))()
   
 elseif game.PlaceId == 6130206252 then -- Mobile Sword Fights
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BigJoeyJoeyFunnies/roux/main/swordfighintg.lua"))()
 
 
   
